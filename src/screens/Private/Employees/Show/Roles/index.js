c9a33b6fe40ca41344d088c0fddf71de1faba714
Maskim Chingin <umnik.dev@gmail.com>
2021-06-28 12:56:10 +0300

    Implement relations between operators and the list of job responsibilities

--- src/screens/Private/Employees/Show/Roles/index.js
+++ src/screens/Private/Employees/Show/Roles/index.js
@@ -2,9 +2,14 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { PATHS } from "utils/constants";
 import { Row, Col } from "antd";
-import { Card, Table } from "shared/components";
+import { Card, Table, Button } from "shared/components";
+import { FaHourglassEnd } from "react-icons/fa";
+import ProtocolsModal from "./protocolsModal";
+import { USER_ROLES } from "shared/constants/userRoles";
+import { useReactiveVar } from "@apollo/client";
+import { UserStore } from "shared/store/UserStore";
 
-const columns = (t) => [
+const columns = (t, protocolsVisible, isAccess) => [
   {
     title: t("SHOW.ROLES.COLUMNS.NAME"),
     dataIndex: ["role", "name"],
@@ -12,7 +17,7 @@ const columns = (t) => [
     sorter: (a, b) => a.role.name.localeCompare(b.role.name),
     render: (name, record) => (
       <Link 
-        className="custom-link" 
+        className="custom-link items-center" 
         to={PATHS.ROLES.SHOW.replace(":id", record.role.id)}>
           {name}
       </Link>
@@ -22,20 +27,46 @@ const columns = (t) => [
     title: t("SHOW.ROLES.COLUMNS.PROTOCOLS"),
     dataIndex: ["role", "protocols"],
     key: "protocols",
-    render: (protocols) => (
-      <div className="access--type">
-        <span className="icon icon-Check green"></span>
-        <span>{t('SHOW.ROLES.COLUMNS.ACCEPTED')}</span>
-      </div>
-    )
+    render: (protocols) => {
+      if (isAccess()) {
+        return (
+          <div className="access--type items-center">
+            <FaHourglassEnd className="icon yellow" />
+            {t('SHOW.ROLES.PENDING')}
+          </div>
+        )
+      } else {
+        return (<Button
+          onClick={() => protocolsVisible(protocols)}
+          icon={<span className="btn--icon--right icon-Eye-Show" />}
+          buttonStyle={"btn--primary"}
+        >{t('SHOW.ROLES.VIEW_PROTOCOLS')}</Button>)
+      }
+    }
   },
 ];
 
 export default ({ t, roles }) => {
+  const user = useReactiveVar(UserStore);
+  const userRole = user && user.issuer && user.issuer.kind ? user.issuer.kind : null;
+
   const [page, setPage] = useState(1);
+  const [protocolsModalVisible, setProtocolsModalVisible] = useState(false);
+  const [protocols, setProtocols] = useState([
+    {name: 'protocol_doc1.pdf', type: 'PDF', updatedAt: '01-01-2020'},
+    {name: 'protocol_doc1.pdf', type: 'PDF', updatedAt: '01-01-2020'},
+  ]);
+
   const onPageChange = (page) => {
     setPage(page);
   };
+
+  const protocolsVisible = (protocols) => {
+    // setProtocols(protocols);
+    setProtocolsModalVisible(true);
+  }
+
+  const isAccess = () => userRole && (userRole === USER_ROLES.CLIENT.key);
 
   return (
     <Card cardStyle={"card--details"}>
@@ -49,7 +80,7 @@ export default ({ t, roles }) => {
       <Row className="w-100-100">
         <Col xs={24} sm={24}>
           <Table
-            columns={columns(t)}
+            columns={columns(t, protocolsVisible, isAccess)}
             className="custom--table"
             data={roles}
             page={page}
@@ -64,6 +95,9 @@ export default ({ t, roles }) => {
           />
         </Col>
       </Row>
+      {protocolsModalVisible 
+        ? <ProtocolsModal t={t} protocols={protocols} visible={protocolsModalVisible} handleCancel={setProtocolsModalVisible} /> 
+        : ''}
     </Card>
   );
 };
