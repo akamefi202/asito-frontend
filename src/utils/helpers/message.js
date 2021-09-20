0b5ffd84a90a5846994e03271e94cb58de73fe51
Max Zhivaev <dizas@me.com>
2021-09-20 11:35:08 +0300

    fixed Error messages

--- src/utils/helpers/message.js
+++ src/utils/helpers/message.js
@@ -1,8 +1,8 @@
 import { message } from "antd";
 
-export const messages = ({data = "", type = "error", msg = "", duration = 2}) => {
+export const messages = ({data = "", type = "error", msg = "", duration = 3}) => {
   if (type === "error" && !msg) {
-    msg = data.message;
+    msg = data.graphQLErrors[0].message ? data.graphQLErrors[0].message : data.message;
   }
 
   return message[type](msg, duration);
